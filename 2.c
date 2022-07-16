a998283682598e697637aeb961643bfd669bb3c4
tik <78137991+wangduoyu666@users.noreply.github.com>
2022-07-16 14:23:08 +0800

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+int a = 2;
+int sum;
+sum=2*a++;
+printf("%d   %d\n",sum,a);
+return 0;
+}
---
+++ 3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+int a = 2;
+int sum;
+printf("%d   %d\n",sum=2*++a,a);
+return 0;
+}
